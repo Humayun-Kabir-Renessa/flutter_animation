1aea49ea6aeceb4fe312846018f87b0e96b091d9
Humayun-Kabir-Renessa <humayun.kabir@renessainfosystems.com>
2021-07-04 09:03:17 +0600

    Favorite icon added. Color Tween and TweenSequence is applied for size

--- lib/components/heart.dart
+++ lib/components/heart.dart
@@ -8,9 +8,9 @@ class Heart extends StatefulWidget {
 }
 
 class _HeartState extends State<Heart> with SingleTickerProviderStateMixin {
-  late AnimationController _controller;
-  late Animation _colorAnimation;
-  late Animation _sizeAnimation;
+  late AnimationController _controller; // Animation Controller
+  late Animation _colorAnimation; // Color Animation
+  late Animation _sizeAnimation; // Size Animation
   bool isFav = false;
   @override
   void initState() {
@@ -19,9 +19,11 @@ class _HeartState extends State<Heart> with SingleTickerProviderStateMixin {
       duration: Duration(milliseconds: 200),
       vsync: this,
     );
+    // Color Tween
     _colorAnimation = ColorTween(begin: Colors.grey[400],end: Colors.red)
     .animate(_controller);
 
+    // TweenSequence is used for size Tween. start from 30 to 50 and back to 30
     _sizeAnimation = TweenSequence(
       <TweenSequenceItem>[
         TweenSequenceItem(
@@ -35,9 +37,11 @@ class _HeartState extends State<Heart> with SingleTickerProviderStateMixin {
       ]
     ).animate(_controller);
 
+    // this method can be used to see the value of animation, means 0 to 1
     _controller.addListener(() {
     });
 
+    // AddStatusListener is used to monitor animation status.
     _controller.addStatusListener((status) {
       if(status == AnimationStatus.dismissed){
         setState(() {
